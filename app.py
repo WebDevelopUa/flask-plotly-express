9b08f0e4c21aa0ae5df8546a23618e2578e6d6b4
Serge M <sergii.mysak@vantino.ch>
2021-07-28 17:35:56 +0300

    Add request info

--- app.py
+++ app.py
@@ -89,8 +89,34 @@ def create_plot_express_csv():
 
 
 # -------------------------------------------------------
+#
+# Request: {
+#     "date_range": {
+#         "type": "monthly",
+#         "year": 2021,
+#         "month": 5,
+#         "day": 12,
+#         "hour": 0
+#     },
+#     "cid": "8546546546146"
+# }
+#
+# Response: {
+#     "nlinks": 10,
+#     "hits": [12, 10, 7, ..., 40],
+#     "top_ref": [
+#                   {"n": "LinkedIn", "h": 12, ... , {"n": "others", "h": 20}}
+#                 ],
+#     "top_cntr": [
+#                   {"n": "Ukraine", "h": 12, ... , {"n": "others", "h": 20}}
+#                 ]
+# }
 
 #  Plotly EXPRESS Sample #3
+# Selector options:
+# Weekly - Display last 7 days
+# Monthly - Display last 1 - 31 days (we need to know what is the current year + month)
+# Yearly - Display all * days (we need to know what is the current year)
 
 
 def create_plot_link_clicks():
@@ -105,7 +131,7 @@ def create_plot_link_clicks():
            '2021-02-08',
            '2021-03-09'],
         y=[10, 20, 100, 50, 0, 1, 55, 25, 150],
-        labels={'y': '', 'x': ''},
+        # labels={'y': '', 'x': ''},
         # title='xc',
     )
 
